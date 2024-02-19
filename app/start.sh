ebcdd41ecde2f4f5a8c38f7fb9ba46029e40b8b8
dtheo91 <dustintheobald@msn.com>
2024-02-19 10:17:16 +0100

    App Changes.

--- app/start.sh
+++ app/start.sh
@@ -3,4 +3,11 @@
 # Run JupyterLab
 jupyter lab --ip=0.0.0.0 --port=8888 --allow-root --no-browser --NotebookApp.token='' --NotebookApp.password='' &
 
-# Run your Python script
+# Wait for JupyterLab to fully initialize (adjust the sleep duration as needed)
+sleep 5
+
+# Run the Python consumer script
+python consumer1.py &
+
+# Keep the container running
+sleep infinity
